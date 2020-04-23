5113a0af8c89700f462315d99f5b8e5b6bdebe4b
Zoltán Varga <zvarga8@dxc.com>
2020-04-23 08:41:37 +0200

    Interface with default implementation and inheritence

---
+++ TryOuts/TryOuts/Interfaces/DefaultImplementation/IInterfaceWithDefaultImplementation.cs
@@ -0,0 +1,12 @@
+﻿namespace TryOuts.Interfaces.DefaultImplementation
+{
+    interface IInterfaceWithDefaultImplementation
+    {
+        int Method();
+
+        string MethodWithDefaultImplementation()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ TryOuts/TryOuts/Interfaces/DefaultImplementation/InterfaceDefaultImplementationClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryOuts.Interfaces.DefaultImplementation
+{
+    class InterfaceDefaultImplementationClass : IInterfaceWithDefaultImplementation
+    {
+        public int Method() => throw new NotImplementedException();
+
+        // This is not necessary to implement
+        //public string MethodWithDefaultImplementation() => throw new NotImplementedException();
+    }
+}
